d9d40d0b32bbad01730148019762420eb922089a
Dmitriy <freedev39@gmail.com>
2025-11-07 22:17:23 +0300

    Создан ConnectDatabase

--- src/app/db/core.py
+++ src/app/db/core.py
@@ -8,18 +8,34 @@ from src.logicinit import config
 settings = config.get_settings()
 
 
-@asynccontextmanager
-async def pool_conn_db():
-    pool = await asyncpg.create_pool(
-        database=settings.POSTGRES_DB,
-        user=settings.POSTGRES_USER,
-        password=settings.POSTGRES_PASSWORD,
-        host=settings.POSTGRES_HOST,
-        port=settings.POSTGRES_PORT,
-        min_size=5,
-        max_size=10
-    )
-    try:
-        yield pool
-    finally:
-        await pool.close()
+class ConnectDatabase:
+    def __init__(self):
+        self.pool = None
+
+    async def connect(self):
+        if not self.pool:
+            self.pool = await asyncpg.create_pool(
+                database=settings.POSTGRES_DB,
+                user=settings.POSTGRES_USER,
+                password=settings.POSTGRES_PASSWORD,
+                host=settings.POSTGRES_HOST,
+                port=settings.POSTGRES_PORT,
+                min_size=5,
+                max_size=10
+            )
+
+    async def disconnect(self):
+        if self.pool is not None:
+            await self.pool.close()
+
+    @asynccontextmanager
+    async def connection(self):
+        conn = await self.pool.acquire()
+        try:
+            yield conn
+        finally:
+            await self.pool.release(conn)
+
+
+connect_db = ConnectDatabase()
+
